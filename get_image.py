97cc189a28d5aa26192bae081a2553be4b833b60
phakorn <phakorn.int@gmail.com>
2022-05-15 13:46:15 +0700

    Version 1.0

--- get_image.py
+++ get_image.py
@@ -1,13 +1,32 @@
 # Import Libralies
 import tableauserverclient as TSC
 import config as cfg
+import urllib3
+urllib3.disable_warnings()
 
 tableau_server = 'https://www.bac.co.th:8000/'
 site_id = 'bacdemo'
 tableau_auth = TSC.TableauAuth(cfg.username,cfg.password,site_id)
 
+# Test Server Connection
 server = TSC.Server(tableau_server)
 server.add_http_options({'verify': False})
 server.use_server_version()
 server.auth.sign_in(tableau_auth)
 print("Logged on Tableau Server")
+
+target_view = 'Dashboard 10'
+def getViewItem(username,password,site_id,target_view):
+    tableau_auth = TSC.TableauAuth(username, password, site_id)
+    with server.auth.sign_in(tableau_auth):
+        all_views = {}
+        for view in TSC.Pager(server.views):
+            all_views[view.name] = view
+        print(all_views)
+    return all_views[target_view]
+
+viewItem = getViewItem(cfg.username,cfg.password,site_id,target_view)
+with server.auth.sign_in(tableau_auth):
+    server.views.populate_image(viewItem)
+    with open('./{}.png'.format(target_view),'wb') as v:
+        v.write(viewItem.image)
